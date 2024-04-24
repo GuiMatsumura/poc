da7a283f1ce51114fcb3af1a2b3eee83cb52e4bb
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2024-04-24 00:42:54 -0300

    Criando Veiculos Controller e gerando o view de forma automática

---
+++ poc/Controllers/VeiculosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using poc.Models;
+
+namespace poc.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
