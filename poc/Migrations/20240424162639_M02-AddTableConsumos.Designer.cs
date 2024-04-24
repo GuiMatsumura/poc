d53c28b0d4eddacca373aab4ce61f369f72d4651
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2024-04-24 13:30:47 -0300

    Adicionando tabela de consumo ao banco de dados juntamente com as migrations

--- poc/Migrations/20240424162639_M02-AddTableConsumos.Designer.cs
+++ poc/Migrations/20240424162639_M02-AddTableConsumos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using poc.Models;
 
@@ -11,9 +12,11 @@ using poc.Models;
 namespace poc.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240424162639_M02-AddTableConsumos")]
+    partial class M02AddTableConsumos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- poc/Models/Veiculo.cs
+++ poc/Models/Veiculo.cs
@@ -22,5 +22,7 @@ namespace poc.Models
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
         [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
     }
 }
