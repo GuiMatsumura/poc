a3247cd90f4e4782759c46d9c30a91966536f75e
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2024-04-24 01:53:45 -0300

    Criando Controller de visualizar os detalhes dos veículos e gerado o view de forma automática

--- poc/Controllers/VeiculosController.cs
+++ poc/Controllers/VeiculosController.cs
@@ -63,5 +63,17 @@ namespace poc.Controllers
 
             return View();
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+            if(dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
     }
 }
