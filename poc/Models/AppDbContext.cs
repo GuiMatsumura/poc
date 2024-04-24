c72d8e9cdd1980c6fab208008fb5377508731704
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2024-04-24 00:24:22 -0300

    Criando banco de dados e a tabela Veiculos

---
+++ poc/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace poc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
